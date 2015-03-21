618089332b9f4acd60f6853a703ef220be3cf493
Siro Díaz Palazón <sirodiaz93@gmail.com>
2015-03-21 18:25:18 +0100

    New methods and documentation in event dispatcher

--- Flyr/Action.php
+++ Flyr/Action.php
@@ -6,32 +6,77 @@ class Action {
 	
 	private $params;
 	
-	public function __construct($actions) {
+	public function __construct($actions, $params = []) {
 		$this->actions = $actions;
+		$this->params = $params;
 	}
 	
 	/**
+	 * Checks if actions are a valid datatype.
 	 * 
+	 * @return bool true if is string, array or not null
 	 */
 	
-	private function execFunction($action) {
+	private function _areValidActions() {
 		
+		if(is_string($this->actions)) {
+			return true;
+		} elseif (is_array($this->actions)) {
+			return true;
+		} elseif(is_callable($this->actions)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 	
 	/**
+	 * Execute the function.
 	 * 
+	 * @param object $action The callback to execute
+	 * @return mixed
+	 */
+	
+	private function _execFunction($action) {
+		return call_user_func_array($action, $this->params);
+	}
+	
+	/**
+	 * Load the class file, create a new instance
+	 * and execute the method.
+	 * 
+	 * @param string $controller The controller name
+	 * @return mixed
+	 */
+	
+	private function _execController($controller) {
+		// check if class file exists
+		// require class
+		// create instance
+		
+		return call_user_func_array([$instance, $methodName], $this->params);
+	}
+	
+	/**
+	 * Ececute the queue of callbacks or controllers
+	 * in the order in which callbacks were defined.
+	 * 
+	 * @return null only if actions are not a
 	 */
 	
 	public function run() {
 		
-		if($this->actions === null) {
-			return;
-		}
-		
-		if (is_array($this->actions)) {
-			foreach ($this->actions as $action) {
-				$this->execFunction($this->actions);
+		if($this->_areValidActions())
+			if (is_array($this->actions)) {
+				foreach ($this->actions as $action) {
+					$this->_execFunction($action);
+				}
+			} elseif(is_callable($this->actions)) {
+				
+			} else {
+				// case action is a string(controller)
+				
 			}
-		} else
+		}
 	}
 }
